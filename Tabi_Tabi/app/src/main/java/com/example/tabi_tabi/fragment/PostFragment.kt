f219e545cba1bb762a27f6571dc1b806b711e20d
MasatakaSakugawa <e185702@u-ryukyu.co.jp>
2020-10-31 17:52:05 +0900

    経度緯度のやつをpostfragmentに渡したよ

--- Tabi_Tabi/app/src/main/java/com/example/tabi_tabi/fragment/PostFragment.kt
+++ Tabi_Tabi/app/src/main/java/com/example/tabi_tabi/fragment/PostFragment.kt
@@ -1,24 +1,25 @@
 package com.example.tabi_tabi.fragment
 
+//import com.example.tabi_tabi.model.TimeLineModel
+
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import com.example.tabi_tabi.R
+import com.example.tabi_tabi.model.PostModel
 import com.google.android.gms.location.LocationResult
-//import com.example.tabi_tabi.model.TimeLineModel
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.FirebaseFirestore
-
 import kotlinx.android.synthetic.main.fragment_post.*
-import java.util.*
-
 
 class PostFragment : BaseFragment() {
 
+    var altitude :Double? = null
+    var latitude :Double? = null
+    var longitude :Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,12 +35,29 @@ class PostFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_post, container, false)
     }
 
+    override fun onLocationResult(locationResult: LocationResult?) {
+        if (locationResult == null) {
+            Log.d(ContentValues.TAG, "# No location data.")
+            return
+        }
+
+        // 緯度・経度を取得
+        latitude = locationResult.lastLocation.latitude
+        longitude = locationResult.lastLocation.longitude
+        altitude = locationResult.lastLocation.altitude
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         button2!!.setOnClickListener {
             val titletext = edit_text1!!.text.toString()
             val posttext = edit_text2!!.text.toString()
+            val location = LatLng(latitude!!,longitude!!)
             val db = FirebaseFirestore.getInstance()
+            val post :PostModel = PostModel(
+                titletext,posttext,location,altitude,"time","content","usrID")
+            )
             //val user = TimeLineModel(1813, titletext, posttext)
 //            db.collection("users")
 //                .document()
@@ -57,16 +75,6 @@ class PostFragment : BaseFragment() {
         activity?.actionBar?.title = "投稿"
     }
 
-    override fun onLocationResult(locationResult: LocationResult?) {
-        if (locationResult == null) {
-            Log.d(ContentValues.TAG, "# No location data.")
-            return
-        }
 
-        // 緯度・経度を取得
-        val latitude = locationResult.lastLocation.latitude
-        Log.d(ContentValues.TAG, "${latitude}")
-        val longitude = locationResult.lastLocation.longitude
-     }
     }
 
